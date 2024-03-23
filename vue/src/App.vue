29a9b1ebc88682134a6e86fd75a12529c46a0304
Stepan Leonov <s.leonov@innopolis.university>
2024-03-23 22:45:51 +0300

    client gprc

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,6 +1,24 @@
 <script setup>
 import CardWrapper from './components/CardWrapper.vue'
 import Navbar from './components/Navbar.vue';
+import { ref, onMounted } from 'vue';
+import { PageRequest, PageReply } from './api/proto/card_pb';
+import ComponentsGetterClient from './api/proto/card_grpc_web_pb'
+
+const page = ref({});
+
+const client = new ComponentsGetterClient("http://localhost:8080", null, null);
+
+const request = new PageRequest();
+//...
+client.getPage(getRequest, {}, (err, response) => {
+  if (response) {
+    page.value = response.toObject();
+    console.log(page.value);
+  }
+});
+
+
 </script>
 
 <template>
