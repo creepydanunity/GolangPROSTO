3f99618ce595817e2f1faa5cbbdff46d1acbb9ed
NPggL <creepydanunity@gmail.com>
2024-03-23 22:56:38 +0300

    fixes

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -2,20 +2,19 @@
 import CardWrapper from './components/CardWrapper.vue'
 import Navbar from './components/Navbar.vue';
 import { ref, onMounted } from 'vue';
-import { PageRequest, PageReply } from './api/proto/card_pb';
-import ComponentsGetterClient from './api/proto/card_grpc_web_pb'
+import { ComponentsGetterClient } from './api/proto/card_grpc_web_pb';
+import { PageReply, PageRequest } from './api/proto/card_pb';
 
 const page = ref({});
 
-const client = new ComponentsGetterClient("http://localhost:8080", null, null);
+const pageService = new ComponentsGetterClient('http://localhost:8080');
 
 const request = new PageRequest();
-//...
-client.getPage(getRequest, {}, (err, response) => {
-  if (response) {
-    page.value = response.toObject();
-    console.log(page.value);
-  }
+request.setMessage('1');
+
+pageService.GetPage(request, {}, function(err, response) {
+  page.value = response.comps;
+  console.log(page.value);
 });
 
 
